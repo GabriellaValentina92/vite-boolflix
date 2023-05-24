ace8d5c161a8bd2ad5169ad5c5bfc4ee50129a1e
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-05-24 16:19:38 +0200

    basic structure

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  components: {},
+};
+</script>
+
+<template></template>
+
+<style scoped></style>
